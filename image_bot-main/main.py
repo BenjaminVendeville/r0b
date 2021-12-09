d83655709ab19ca7b79ce82cc63cecd36b9fc9e8
BenjaminVendeville <ben.vendeville@orange.fr>
2021-12-09 16:47:53 +0100

    base commit

---
+++ image_bot-main/main.py
@@ -0,0 +1,25 @@
+import discord
+from discord.ext import commands
+
+import os
+
+#import all of the cogs
+from main_cog import main_cog
+from image_cog import image_cog
+from music_cog import music_cog
+
+bot = commands.Bot(command_prefix='/')
+
+TOKEN = "OTE4MDczMTU5NzQ2NDg2MzAz.YbB8Qg.BMZHo82hHoEUR1IPtQrfsC7wKZc"
+
+#remove the default help command so that we can write out own
+bot.remove_command('help')
+
+#register the class with the bot
+bot.add_cog(main_cog(bot))
+#bot.add_cog(image_cog(bot))
+bot.add_cog(music_cog(bot))
+
+#start the bot with our token
+bot.run(TOKEN)
+
